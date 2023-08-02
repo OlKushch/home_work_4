85e442ac1b7c75e71d9e3701977882df37749c80
Olga Belyaeva <olgakushch96@gmail.com>
2023-08-02 15:23:31 +0300

    Домашняя работа к 4 уроку

---
+++ homework_for_4_less.py
@@ -0,0 +1,107 @@
+import math
+import random
+
+
+def test_greeting():
+    """
+    Напишите программу, которая выводит на экран приветствие.
+    """
+    name = "Анна"
+    age = 25
+    # TODO Сформируйте нужную строку
+    output = "Привет, " + name + "! Тебе " + str(age) + " лет."
+    # Проверяем результат
+    assert output == "Привет, Анна! Тебе 25 лет."
+    print(output)
+
+
+def test_rectangle():
+    """
+    Напишите программу, которая берет длину и ширину прямоугольника
+    и считает его периметр и площадь.
+    """
+    a = 10
+    b = 20
+
+    # TODO сосчитайте периметр
+    perimeter = (a + b) * 2
+    assert perimeter == 60
+    print(perimeter)
+
+    # TODO сосчитайте площадь
+    area = a * b
+    assert area == 200
+    print(area)
+
+
+def test_circle():
+    """
+    Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
+    Используйте константу PI
+    """
+    r = 23
+    # TODO сосчитайте площадь
+    area = r * r * math.pi
+    assert area == 1661.9025137490005
+    print(area)
+
+    # TODO сосчитайте длину окружности
+    length = r * 2 * math.pi
+    assert length == 144.51326206513048
+    print(length)
+
+
+def test_random_list():
+    """
+    Создайте список из 10 случайных чисел от 1 до 100 и отсортируйте его по возрастанию.
+    """
+
+    # TODO создайте список
+    l = []
+    for i in range(10):
+        l1 = random.randint(1, 101)
+        l.append(l1)
+        l.sort()
+    print(l)
+    assert len(l) == 10
+    assert l[0] < l[-1]
+
+
+
+def test_unique_elements():
+    """
+    Удалите из списка все повторяющиеся элементы
+    """
+    l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
+    # TODO удалите повторяющиеся элементы
+    l = list(set(l))
+    print(type(l), len(l))
+
+
+    assert isinstance(l, list)
+    assert len(l) == 10
+    assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_dicts():
+    """
+    Создайте словарь из двух списков.
+    Используйте первый список как ключи, а второй - как значения.
+    Выведите на экран все значения словаря.
+    Подсказка: используй встроенную функцию zip.
+    """
+    first = ["a", "b", "c", "d", "e"]
+    second = [1, 2, 3, 4, 5]
+    # TODO создайте словарь
+    d = dict(zip(first, second))
+    print(d,type(d), len(d))
+
+    assert isinstance(d, dict)
+    assert len(d) == 5
+
+test_greeting()
+test_rectangle()
+test_circle()
+test_random_list()
+test_unique_elements()
+test_dicts()
